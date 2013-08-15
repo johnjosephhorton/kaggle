d21f54fbf94e530ed88f6932e4e96238cd2056c2
John Horton <john.joseph.horton@gmail.com>
2013-08-15 16:00:45 -0700

    Added GBM approach - Rank:3732	PCP: 0.76555 Thu, 15 Aug 2013 22:55:51

--- titantic/models/predict_survival.R
+++ titantic/models/predict_survival.R
@@ -1,26 +1,60 @@
 library(imputation)
 library(ggplot2)
+library(mlbench)
+library(caret)
 
 df.train.raw <- read.csv("../data/train.csv")
+df.train <- df.train.raw
 
-predictors <- c("Pclass", "Sex", "Age", "Fare", "PassengerId")
-outcome <- c("Survived")
-df.train <- df.raw[, c(predictors, outcome)]
+########################################
+# PRE-PROCESS DATA / FEATURE ENGINEERING
+########################################
 
-survival.rate <- mean(df.train$Survived)
+ship.regions <- c("A", "B", "C", "D", "E", "F", "G", "H")
+get.region <- function(cabin){
+    x1 <- gsub("[0-9]", "", cabin)
+    x2 <- gsub(" ", "", x1)
+    x2
+    substring(x2, 1,1)
+}
 
-df.train <- na.omit(df.train)
+create.features <- function(df){
+    df$region <- with(df, sapply(Cabin, get.region))
+    df <- within(df,{
+                   has.cabin <- as.numeric((Cabin != ""))
+                   irish <- grepl("Mc", Name) | grepl("O\'", Name)
+                   married <- grepl("Mrs", Name)
+                   name.length <- nchar(as.character(Name))
+                   ship.loc <- ifelse(region %in% c("A", "B", "C", "D", "E", "F"), region, "Other")
+               })
+    df
+}
 
-# Fit model 
-m <- glm(Survived ~ factor(Pclass)*factor(Sex) + poly(Age, 3) +
-         log1p(Fare), data = df, family = "binomial")
 
-# load test data 
-df.test.raw <- read.csv("../data/test.csv")
-df.test <- df.test.raw[, predictors]
-summary(df.test)
+df.train <- create.features(df.train.raw)
 
-# Impute missing data 
+###############
+# FIT THE MODEL
+###############
+
+df.train <- df.train[, c("Survived", "Age", "Fare", "Pclass", "Sex", "has.cabin", "Parch", "SibSp", 
+                                      "irish", "name.length", "ship.loc", "Embarked")] 
+
+fitControl <- trainControl(method = "repeatedcv", number = 10, repeats = 10)
+
+gbmFit1 <- train(Survived ~ ., data = df.train,
+                 method = "gbm",
+                 trControl = fitControl, verbose = FALSE)
+
+##################
+# GET TESTING DATA
+##################
+df.test <- create.features(read.csv("../data/test.csv"))
+
+
+######################
+# Age imputation model
+#######################
 m.age <- lm(Age ~ Pclass + Sex +  Fare, data =  merge(df.train, df.test))
 df.test$Age.hat <- predict(m.age, newdata = df.test)
 df.test$Age <- with(df.test, ifelse(is.na(Age), Age.hat, Age))
@@ -29,9 +63,10 @@ df.test$Age.hat <- NULL
 
 summary(df.test)
 
+##################
+# Make Predictions 
+##################
 
-df.test$Survived <- ifelse(predict(m, newdata = df.test, type = "response") > survival.rate, 1, 0) 
-
-
-write.csv(df.test[,c("PassengerId", "Survived")], "../predictions/predictions_15AUG13.csv", row.names = FALSE)
+df.test$Survived <- ifelse(predict(gbmFit1, newdata = df.test) > survival.rate, 1, 0) 
+write.csv(df.test[,c("PassengerId", "Survived")], "../predictions/predictions_15AUG13_3.csv", row.names = FALSE)
 
